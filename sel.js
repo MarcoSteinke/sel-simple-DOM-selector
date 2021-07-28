eb9384a10561dced7b4dbc82dc48b3a3e4537170
Marco Steinke <49523050+MarcoSteinke@users.noreply.github.com>
2021-07-28 15:56:13 +0200

    Update sel.js

--- sel.js
+++ sel.js
@@ -3,8 +3,8 @@ const sl = (selector, root = document) => root.querySelector(selector);
 // Select all elements with specific selector from DOM
 const sla = (selector, root = document) => root.querySelectorAll(selector);
 // Select n-th element with specific selector from DOM
-const sln = (selector, n, root = document) => root.querySelectorAll(selector)[n];
+const sln = (selector, n, root = document) => sla(selector)[n];
 // Select first element with specific selector from DOM
-const slf = (selector, root = document) => root.querySelectorAll(selector)[0];
+const slf = (selector, root = document) => sla(selector, root)[0];
 // Select last element with specific selector from DOM
-const sll = (selector, root = document) => root.querySelectorAll(selector)[root.querySelectorAll(selector).length - 1];
+const sll = (selector, root = document) => sla(selector, root)[sla(selector, root).length - 1];
