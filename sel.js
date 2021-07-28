37eef21068cf10d38366a22a15c79f0f39b04180
Marco Steinke <49523050+MarcoSteinke@users.noreply.github.com>
2021-07-28 14:55:44 +0200

    Create sel.js

---
+++ sel.js
@@ -0,0 +1,10 @@
+// Select an element from DOM
+const sel = (selector) => document.querySelector(selector);
+// Select all elements with specific selector from DOM
+const selA = (selector) => document.querySelectorAll(selector);
+// Select n-th element with specific selector from DOM
+const selN = (selector, n) => document.querySelectorAll(selector)[n];
+// Select first element with specific selector from DOM
+const selF = (selector) => document.querySelectorAll(selector)[0];
+// Select last element with specific selector from DOM
+const selL = (selector) => document.querySelectorAll(selector)[document.querySelectorAll.length - 1];
