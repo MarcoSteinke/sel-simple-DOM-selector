67393b8837bcc3831749685d5eb640c79979d027
Marco Steinke <49523050+MarcoSteinke@users.noreply.github.com>
2021-07-29 09:26:57 +0200

    Update sel.js

--- sel.js
+++ sel.js
@@ -1,4 +1,4 @@
-const [sl, sla, sln, slf, sll] = [
+const [sl, sla, sln, slf, sll, slfn, slln] = [
   // Select an element from DOM
   (selector, root = document) => root.querySelector(selector),
   
@@ -12,5 +12,11 @@ const [sl, sla, sln, slf, sll] = [
   (selector, root = document) => sla(selector, root)[0],
   
   // Select last element with specific selector from DOM
-  (selector, root = document) => sla(selector, root)[sla(selector, root).length - 1]
+  (selector, root = document) => sla(selector, root)[sla(selector, root).length - 1],
+  
+  // Select first n elements with specific selector from DOM
+  (selector, n, root = document) => sla(selector, root).slice(0, n),
+  
+  // Select last n elements with a specific selector from DOM
+  (selector, n, root = document) => sla(selector, root).slice(sla(selector, root).length - n, sla(selector, root).length)
 ]
